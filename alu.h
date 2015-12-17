fe1de3f200ebd273ebf8440a54eba2899b11035f
martinfred <martinfray@orange.fr>
2015-12-17 12:33:14 +0100

    suppression fichiers inutile pour v1

--- alu.h
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-#include <limits.h>
-
-int ADD(int * rd, int rs, int rt);
-
-
--- registers.h
+++
@@ -1,5 +0,0 @@
-
-
-void registersWrite(int r, int n);
-int registersRead(int r);
-
