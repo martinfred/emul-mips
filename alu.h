88d8e22f5f885fb21704611c3e8f877b5e9f16f6
ourakou <quentin.fritayre@gmail.com>
2015-12-21 18:16:01 +0100

    Update alu.h

--- alu.h
+++ alu.h
@@ -3,5 +3,7 @@
 #include <limits.h>
 
 int ADD(int * rd, int rs, int rt);
-
-
+int ADDI(int * rd, int rs, int i);
+int SUB(int *rd, int rs, int rt);
+int MULT(int *HI, int *LO, int rs, int rt);
+int DIV(int *HI, int *LO, int rs, int rt);
