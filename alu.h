e55d6e257e0367d21fd36686e83ee1e10942b0e9
martinfred <martinfray@orange.fr>
2016-01-13 12:13:12 +0100

    descrption.txt

--- alu.h
+++ alu.h
@@ -5,8 +5,8 @@
 #include "registers.h"
 
 
-int ADD(int * rd, int rs, int rt);
-int ADDI(int * rd, int rs, int i);
+int ADD(int *rd, int rs, int rt);
+int ADDI(int *rd, int rs, int i);
 int SUB(int *rd, int rs, int rt);
 int MULT(int rs, int rt);
 int DIV(int rs, int rt);
